d5aa565985298f01e0089f3ebf5bdd4bcb5a810d
SoufiMouhcine <you@example.com>
2021-02-25 17:54:39 +0100

    service user signup

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,18 @@
 import * as axios from 'axios';
 
 
-const register = async function() {
-    const response = await axios.post('localhost:3000/users/signin')
+const register = async function(user) {
+    const response = await axios.post('localhost:3000/users/signin', user);
+    parseItem(response, 201);
+
+
 }
+
+export const parseItem = (response, code) => {
+    if (response.status !== code) throw Error(response.message);
+    let item = response.data;
+    if (typeof item !== 'object') {
+        item = undefined;
+    }
+    return item;
+};
