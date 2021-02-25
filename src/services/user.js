45ba0b3ea057592b1bf6837877f792ca7b4f6db0
SoufiMouhcine <you@example.com>
2021-02-25 18:01:27 +0100

    paresItem

--- src/services/user.js
+++ src/services/user.js
@@ -2,11 +2,15 @@ import * as axios from 'axios';
 
 
 const register = async function(user) {
-    const response = await axios.post('localhost:3000/users/signin', user);
-    parseItem(response, 201);
-
-
-}
+    try {
+        const response = await axios.post('localhost:3000/users/signin', user);
+        const addeduser = parseItem(response, 201);
+        return addeduser;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
 
 export const parseItem = (response, code) => {
     if (response.status !== code) throw Error(response.message);
@@ -16,3 +20,7 @@ export const parseItem = (response, code) => {
     }
     return item;
 };
+
+module.exports = {
+    register,
+}
