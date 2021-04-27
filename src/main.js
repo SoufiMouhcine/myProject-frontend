c56c31289caf563e6a3b281b2097ab1f7cd5a280
SoufiMouhcine <mohciness3@gmail.com>
2021-04-27 14:41:16 +0200

    google_auth_firebase

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import { firestorePlugin } from 'vuefire';
+Vue.use(firestorePlugin);
+
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -62,8 +62,31 @@
             />
           </div>
           <br />
-          <button class="btn-lg btn-primary">Register</button>
+          <button class="btn-lg btn-primary btn-block">Register</button>
         </form>
+        <div class="or-container">
+          <div class="line-separator"></div>
+          <div class="or-label">or</div>
+          <div class="line-separator"></div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <button
+              class="btn btn-lg btn-google btn-block btn-outline"
+              @click="firebaseLogin"
+            >
+              <img
+                src="https://img.icons8.com/color/16/000000/google-logo.png"
+              />
+              Signup Using Google
+            </button>
+          </div>
+        </div>
+        <br />
+        <p class="text-inverse text-center">
+          Already have an account?
+          <a href="<?= base_url() ?>auth/login" data-abc="true">Login</a>
+        </p>
       </div>
       <div class="col-md-3"></div>
     </div>
@@ -71,7 +94,17 @@
 </template>
 
 <script>
+  var firebaseConfig = {
+    apiKey: "AIzaSyCU0vVShMAyQ9lW93mgx5FGfj-_myu9tzg",
+    authDomain: "auth-3cebc.firebaseapp.com",
+    projectId: "auth-3cebc",
+    storageBucket: "auth-3cebc.appspot.com",
+    messagingSenderId: "499265597059",
+    appId: "1:499265597059:web:3e96d084106f85a51e75b6"
+  };
+  // Initialize Firebase
 import { mapActions } from "vuex";
+import Firebase from 'firebase';
 export default {
   name: "register",
   data() {
@@ -85,6 +118,10 @@ export default {
       },
     };
   },
+  created() {
+    if (!Firebase.apps.length)
+      this.app = Firebase.initializeApp(firebaseConfig);
+  },
   methods: {
     ...mapActions(["registerAction"]),
     async register() {
@@ -94,12 +131,52 @@ export default {
         alert("Password length should be greater than 6");
       } else {
         await this.registerAction(this.user);
-        
       }
+    },
+    firebaseLogin() {
+      const provider = new Firebase.auth.GoogleAuthProvider();
+Firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user)
+    console.log("---->"+token)
+    // ...
+  })
     },
   },
 };
 </script>
 
 <style scoped>
+.btn-google {
+  color: #545454;
+  background-color: #fffffb;
+  box-shadow: 0 1px 1px 1px #ddd;
+}
+
+.or-container {
+  align-items: center;
+  color: #ccc;
+  display: flex;
+  margin: 25px 0;
+}
+
+.line-separator {
+  background-color: #ccc;
+  flex-grow: 5;
+  height: 1px;
+}
+
+.or-label {
+  flex-grow: 1;
+  margin: 0 15px;
+  text-align: center;
+}
 </style>
